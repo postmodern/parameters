153439e53ac1d079a4772736463042bf4665c319
postmodern <postmodern.mod3@gmail.com>
2008-12-02 17:02:19 -0800

    Added a spec for the new Parameters#initialize method.

--- spec/parameters_spec.rb
+++ spec/parameters_spec.rb
@@ -55,6 +55,13 @@ describe Parameters do
     it "should provide descriptions for parameters" do
       TestParameters.describe_param(:var).should_not be_empty
     end
+
+    it "should be able to create an object with initial parameter values" do
+      obj = TestParameters.new(:var => 2, :var_with_default => 'stuff')
+
+      obj.var.should == 2
+      obj.var_with_default.should == 'stuff'
+    end
   end
 
   describe "in an Object" do
