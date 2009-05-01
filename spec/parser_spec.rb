e68fde33700da48cf97f3ede12cf72751036243b
postmodern <postmodern.mod3@gmail.com>
2009-04-30 19:53:55 -0700

    Updated specs for Parameters::Parser.

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,47 +2,39 @@ require 'parameters/parser'
 
 require 'spec_helper'
 
-describe Parser do
-  before(:all) do
-    @command = ExampleCommand.new('example')
+describe Parameters::Parser do
+  it "should parse string values" do
+    Parameters::Parser.parse_value("'bla \\' \\\" bla'").should == "bla ' \\\" bla"
+  end
+
+  it "should parse decimal values" do
+    Parameters::Parser.parse_value("100").should == 100
+  end
+
+  it "should parse octal values" do
+    Parameters::Parser.parse_value("012").should == 10
+  end
+
+  it "should parse hexadecimal values" do
+    Parameters::Parser.parse_value("0xff").should == 0xff
+  end
+
+  it "should parse boolean values" do
+    Parameters::Parser.parse_value('true').should == true
+    Parameters::Parser.parse_value('false').should == false
+  end
+
+  it "should parse URI values" do
+    url = 'http://example.com/'
+
+    Parameters::Parser.parse_value(url).should == URI(url)
   end
 
   it "should parse params of the form 'name'" do
-    @command.param_parser('var')
-
-    @command.params.has_key?(:var).should == true
-    @command.params[:var].should be_nil
+    Parameters::Parser.parse_param('var').should == {:var => nil}
   end
 
   it "should parse params of the form 'name=value'" do
-    @command.param_parser('var1=test')
-    @command.params[:var1].should == 'test'
-  end
-
-  it "should parse params which have numeric values" do
-    @command.param_parser('var2=100')
-    @command.params[:var2].should == 100
-  end
-
-  it "should parse params with hexadecimal values" do
-    @command.param_parser('var3=0x2a')
-    @command.params[:var3].should == 0x2a
-  end
-
-  it "should parse params with URI values" do
-    url = 'http://example.com/'
-
-    @command.param_parser("var4=#{url}")
-    @command.params[:var4].should == URI(url)
-  end
-
-  it "should parse params with true boolean values" do
-    @command.param_parser('var5=true')
-    @command.params[:var5].should == true
-  end
-
-  it "should parse params with false boolean values" do
-    @command.param_parser('var6=false')
-    @command.params[:var6].should == false
+    Parameters::Parser.parse_param('var1=test').should == {:var1 => 'test'}
   end
 end
