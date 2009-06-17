860cbf97b91744c228fb869f710ad32148060635
postmodern <postmodern.mod3@gmail.com>
2009-06-17 15:25:11 -0700

    Spacing.

--- spec/classes/other_parameters.rb
+++ spec/classes/other_parameters.rb
@@ -4,7 +4,7 @@ class OtherParameters
   parameter :var, :description => 'Test parameter'
 
   parameter :var_with_default,
-    :default => 'other',
-    :description => 'This parameter has a default value'
+            :default => 'other',
+            :description => 'This parameter has a default value'
 
 end
