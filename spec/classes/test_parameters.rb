b95c61aac32d6f9e8bb2a0f6021a26f86a912736
postmodern <postmodern.mod3@gmail.com>
2009-06-17 15:25:32 -0700

    Spacing.

--- spec/classes/test_parameters.rb
+++ spec/classes/test_parameters.rb
@@ -4,13 +4,13 @@ class TestParameters
   parameter :var, :description => 'Test parameter'
 
   parameter :var_with_default,
-    :default => 'thing',
-    :description => 'This parameter has a default value'
+            :default => 'thing',
+            :description => 'This parameter has a default value'
 
   parameter :var_without_default,
-    :description => 'This parameter does not have a default value'
+            :description => 'This parameter does not have a default value'
 
   parameter :var_with_lambda,
-    :default => lambda { rand },
-    :description => 'This parameter uses a lambda instead of a default value'
+            :default => lambda { rand },
+            :description => 'This parameter uses a lambda instead of a default value'
 end
