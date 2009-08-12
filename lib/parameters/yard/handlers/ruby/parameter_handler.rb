bd0ea23155c2a202ad8f9e48ebce1f426effb498
postmodern <postmodern.mod3@gmail.com>
2009-08-12 15:23:51 -0700

    Only detect method_call(:parameter).
    * There is no :parameter keyword.

--- lib/parameters/yard/handlers/ruby/parameter_handler.rb
+++ lib/parameters/yard/handlers/ruby/parameter_handler.rb
@@ -5,24 +5,19 @@ module YARD
     module Ruby
       class ParameterHandler < Base
 
-        handles :parameter, method_call(:parameter)
+        handles method_call(:parameter)
 
         def process
+          obj = statement.parameters(false).first
           nobj = namespace
           mscope = scope
-          name = if statement.type == :parameter
-                   statement.jump(:ident, :op, :kw, :const).source
-                 elsif statement.call?
-                   obj = statement.parameters(false).first
-
-                   case obj.type
+          name = case obj.type
                    when :symbol_literal
                      obj.jump(:ident, :op, :kw, :const).source
                    when :string_literal
                      obj.jump(:string_content).source
                    end
                  end
-
 
           register MethodObject.new(nobj, name, mscope) do |o|
             o.visibility = :public
