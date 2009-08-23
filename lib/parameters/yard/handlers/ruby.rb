8db58e93ad381c46dec7d469d9adcced294862f7
postmodern <postmodern.mod3@gmail.com>
2009-08-22 17:08:50 -0700

    Added handlers for metaclass_eval.

--- lib/parameters/yard/handlers/ruby.rb
+++ lib/parameters/yard/handlers/ruby.rb
@@ -1,3 +1,4 @@
 require 'parameters/yard/handlers/ruby/eval_block_handler'
+require 'parameters/yard/handlers/ruby/metaclass_eval_handler'
 require 'parameters/yard/handlers/ruby/parameter_handler'
 require 'parameters/yard/handlers/ruby/legacy'
--- lib/parameters/yard/handlers/ruby/legacy.rb
+++ lib/parameters/yard/handlers/ruby/legacy.rb
@@ -1,2 +1,3 @@
 require 'parameters/yard/handlers/ruby/legacy/eval_block_handler'
+require 'parameters/yard/handlers/ruby/legacy/metaclass_eval_handler'
 require 'parameters/yard/handlers/ruby/legacy/parameter_handler'
