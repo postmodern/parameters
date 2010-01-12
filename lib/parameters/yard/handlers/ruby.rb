de058ed8c5bd3d5a310b5ec969b084bced88f5c4
postmodern <postmodern.mod3@gmail.com>
2010-01-12 00:51:57 -0800

    Deleted the metaclass_eval YARD handler.

--- lib/parameters/yard/handlers/ruby.rb
+++ lib/parameters/yard/handlers/ruby.rb
@@ -1,4 +1,3 @@
 require 'parameters/yard/handlers/ruby/eval_block_handler'
-require 'parameters/yard/handlers/ruby/metaclass_eval_handler'
 require 'parameters/yard/handlers/ruby/parameter_handler'
 require 'parameters/yard/handlers/ruby/legacy'
--- lib/parameters/yard/handlers/ruby/legacy.rb
+++ lib/parameters/yard/handlers/ruby/legacy.rb
@@ -1,3 +1,2 @@
 require 'parameters/yard/handlers/ruby/legacy/eval_block_handler'
-require 'parameters/yard/handlers/ruby/legacy/metaclass_eval_handler'
 require 'parameters/yard/handlers/ruby/legacy/parameter_handler'
