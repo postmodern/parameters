8e4dfa2f0825884a62d11c4d6dcc926d4af492c6
postmodern <postmodern.mod3@gmail.com>
2010-02-17 20:34:09 -0800

    Use symbols for param names.

--- spec/class_param_spec.rb
+++ spec/class_param_spec.rb
@@ -6,70 +6,70 @@ require 'set'
 describe Parameters::ClassParam do
   describe "type coersion" do
     it "should coerce Arrays" do
-      param = Parameters::ClassParam.new('x',Array)
+      param = Parameters::ClassParam.new(:x,Array)
 
       param.value = Set[1, 2, 3]
       param.value.should == [1, 2, 3]
     end
 
     it "should coerce Arrays with types" do
-      param = Parameters::ClassParam.new('x',Array[Integer])
+      param = Parameters::ClassParam.new(:x,Array[Integer])
 
       param.value = Set['1', '2', '3']
       param.value.should == [1, 2, 3]
     end
 
     it "should coerce URIs" do
-      param = Parameters::ClassParam.new('x',URI)
+      param = Parameters::ClassParam.new(:x,URI)
 
       param.value = 'http://bla.com/'
       param.value.should == URI::HTTP.build(:host => 'bla.com')
     end
 
     it "should coerce Symbols" do
-      param = Parameters::ClassParam.new('x',Symbol)
+      param = Parameters::ClassParam.new(:x,Symbol)
 
       param.value = 'str'
       param.value.should == :str
     end
 
     it "should coerce Strings" do
-      param = Parameters::ClassParam.new('x',String)
+      param = Parameters::ClassParam.new(:x,String)
 
       param.value = :str
       param.value.should == 'str'
     end
 
     it "should coerce Floats" do
-      param = Parameters::ClassParam.new('x',Float)
+      param = Parameters::ClassParam.new(:x,Float)
 
       param.value = '0.5'
       param.value.should == 0.5
     end
 
     it "should coerce Integers" do
-      param = Parameters::ClassParam.new('x',Integer)
+      param = Parameters::ClassParam.new(:x,Integer)
 
       param.value = '5'
       param.value.should == 5
     end
 
     it "should coerce hex Integers" do
-      param = Parameters::ClassParam.new('x',Integer)
+      param = Parameters::ClassParam.new(:x,Integer)
 
       param.value = '0xa'
       param.value.should == 10
     end
 
     it "should coerce octal Integers" do
-      param = Parameters::ClassParam.new('x',Integer)
+      param = Parameters::ClassParam.new(:x,Integer)
 
       param.value = '010'
       param.value.should == 8
     end
 
     it "should not coerce unknown types" do
-      param = Parameters::ClassParam.new('x')
+      param = Parameters::ClassParam.new(:x)
       obj1 = Object.new
 
       param.value = obj1
--- spec/instance_param_spec.rb
+++ spec/instance_param_spec.rb
@@ -10,70 +10,70 @@ describe Parameters::InstanceParam do
     end
 
     it "should coerce Arrays" do
-      param = Parameters::InstanceParam.new(@obj,'x',Array)
+      param = Parameters::InstanceParam.new(@obj,:x,Array)
 
       param.value = Set[1, 2, 3]
       param.value.should == [1, 2, 3]
     end
 
     it "should coerce Arrays with types" do
-      param = Parameters::InstanceParam.new(@obj,'x',Array[Integer])
+      param = Parameters::InstanceParam.new(@obj,:x,Array[Integer])
 
       param.value = Set['1', '2', '3']
       param.value.should == [1, 2, 3]
     end
 
     it "should coerce URIs" do
-      param = Parameters::InstanceParam.new(@obj,'x',URI)
+      param = Parameters::InstanceParam.new(@obj,:x,URI)
 
       param.value = 'http://bla.com/'
       param.value.should == URI::HTTP.build(:host => 'bla.com')
     end
 
     it "should coerce Symbols" do
-      param = Parameters::InstanceParam.new(@obj,'x',Symbol)
+      param = Parameters::InstanceParam.new(@obj,:x,Symbol)
 
       param.value = 'str'
       param.value.should == :str
     end
 
     it "should coerce Strings" do
-      param = Parameters::InstanceParam.new(@obj,'x',String)
+      param = Parameters::InstanceParam.new(@obj,:x,String)
 
       param.value = :str
       param.value.should == 'str'
     end
 
     it "should coerce Floats" do
-      param = Parameters::InstanceParam.new(@obj,'x',Float)
+      param = Parameters::InstanceParam.new(@obj,:x,Float)
 
       param.value = '0.5'
       param.value.should == 0.5
     end
 
     it "should coerce Integers" do
-      param = Parameters::InstanceParam.new(@obj,'x',Integer)
+      param = Parameters::InstanceParam.new(@obj,:x,Integer)
 
       param.value = '5'
       param.value.should == 5
     end
 
     it "should coerce hex Integers" do
-      param = Parameters::InstanceParam.new(@obj,'x',Integer)
+      param = Parameters::InstanceParam.new(@obj,:x,Integer)
 
       param.value = '0xa'
       param.value.should == 10
     end
 
     it "should coerce octal Integers" do
-      param = Parameters::InstanceParam.new(@obj,'x',Integer)
+      param = Parameters::InstanceParam.new(@obj,:x,Integer)
 
       param.value = '010'
       param.value.should == 8
     end
 
     it "should not coerce unknown types" do
-      param = Parameters::InstanceParam.new(@obj,'x')
+      param = Parameters::InstanceParam.new(@obj,:x)
       obj1 = Object.new
 
       param.value = obj1
