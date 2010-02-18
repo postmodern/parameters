a9df6dfcaf8d3d710aedbaa0b0011de9ecd18de5
postmodern <postmodern.mod3@gmail.com>
2010-02-17 21:25:26 -0800

    Added specs for InstanceParam#value and InstanceParam#value=.

--- spec/instance_param_spec.rb
+++ spec/instance_param_spec.rb
@@ -4,6 +4,22 @@ require 'spec_helper'
 require 'set'
 
 describe Parameters::InstanceParam do
+  it "should read values from another object" do
+    obj = Object.new
+    param = Parameters::InstanceParam.new(obj,:x)
+
+    obj.instance_variable_set(:"@x",5)
+    param.value.should == 5
+  end
+
+  it "should write values to another object" do
+    obj = Object.new
+    param = Parameters::InstanceParam.new(obj,:x)
+
+    param.value = 5
+    obj.instance_variable_get(:"@x").should == 5
+  end
+
   describe "type coersion" do
     before(:each) do
       @obj = Object.new
